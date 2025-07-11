1b68cde2f3bad26e701e391dcc50599ca30ef761
Juan Valdiviezo <juanvaldiviezo@MacBook-Air-de-Juan.local>
2025-07-10 23:44:09 -0500

    carga de catalgos dialogo

--- src/app/pages/productos/producto-dialogo/producto-dialogo.ts
+++ src/app/pages/productos/producto-dialogo/producto-dialogo.ts
@@ -1,17 +1,31 @@
 import { Component,Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+interface TipoMaquina {
+  IdTipoMaquina: number;
+  CodigoMaquina: string;
+  Nombre: string;
+}
 
 @Component({
   standalone: true,
   selector: 'app-producto-dialogo',
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './producto-dialogo.html',
   styleUrl: './producto-dialogo.css'
 })
 export class ProductoDialogo {
  @Input() producto: any;
+  maquinas: TipoMaquina[] = [];
+  private apiUrl = 'http://127.0.0.1:8000/api';
 
 
+ constructor(private http: HttpClient) {
+    this.cargarTiposMaquina();
+  }
  actualizarTiempoTotal() {
   const horas = this.producto?.horas ?? 0;
   const minutos = this.producto?.minutos ?? 0;
@@ -30,5 +44,17 @@ export class ProductoDialogo {
 
   this.producto.tiempoTotal = `${totalHoras}h ${totalRestMin}m`;
 }
+  cargarTiposMaquina() {
+    this.http.get<TipoMaquina[]>(`${this.apiUrl}/tipos-maquina`)
+      .subscribe({
+        next: (response) => {
+          this.maquinas = response;
+          console.log('Tipos de máquina cargados dialogo:', response);
+        },
+        error: (error) => {
+          console.error('Error cargando tipos de máquina:', error);
+        }
+      });
+  }
 
 }
