850b3411479ea20c604d67176cf75183319ec604
Juan Valdiviezo <juanvaldiviezo@MacBook-Air-de-Juan.local>
2025-07-15 17:27:57 -0500

    correcion refresco de pantallas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,37 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-import { Index } from './pages/index';
-import { AuthGuard } from './auth/auth-guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: Login },
-    {
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/content/content').then(m => m.Content),
+    children: [
+      {
         path: 'index',
         loadComponent: () =>
-            import('./pages/index/index').then(m => m.Index)
-    },
-    {
+          import('./pages/index/index').then(m => m.Index)
+      },
+      {
         path: 'productos',
         loadComponent: () =>
-            import('./pages/productos/productos').then(m => m.Productos)
-    },
-    {
+          import('./pages/productos/productos').then(m => m.Productos)
+      },
+      {
         path: 'configuracion',
         loadComponent: () =>
-            import('./pages/costoTiempo/configuracion').then(m => m.Configuracion)
-    },{
+          import('./pages/costoTiempo/configuracion').then(m => m.Configuracion)
+      },
+      {
         path: 'generador-cotizacion',
         loadComponent: () =>
-            import('./pages/generador-cotizacion/generador-cotizacion').then(m => m.GeneradorCotizacion)
-    }
-    
+          import('./pages/generador-cotizacion/generador-cotizacion').then(
+            m => m.GeneradorCotizacion
+          )
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'index' }
 ];
--- src/app/layout/content/content.ts
+++ src/app/layout/content/content.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Auth } from '../../auth/auth';
 import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar'; 
+
 
 @Component({
   standalone: true,
   selector: 'app-content',
-  imports: [],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
